82be346ea02c91591504f7be0009064e16f8a420
Roman Usatov <R.Usatov76@gmail.com>
2019-04-14 18:58:40 +0300

    Homework #2

---
+++ modals hard #2/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-content">
+
+      <section>
+        <div class="container">
+
+          <!-- Login -->
+          <button class="btn btnPrimary" @click="showModal = 'login'">Login</button>
+          <login v-show="showModal === 'login'" @close="showModal = 'close'"/>
+          
+          <!-- Registration -->
+          <button class="btn btnPrimary" @click="showModal = 'registration'">Registration</button>
+          <registration v-show="showModal === 'registration'" @close="showModal = 'close'"/>
+
+        </div>
+      </section>
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+import modals from '@/components/UI/Modal.vue'
+import login from '@/components/Login.vue'
+import registration from '@/components/Registration.vue'
+
+export default {
+  components: { modals, login, registration },
+  data () {
+    return {
+      showModal: 'close'
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
---
+++ modals hard #2/src/components/Registration.vue
@@ -0,0 +1,125 @@
+<template>
+  <modal title="Registration" @close="clearField">
+    <!-- body -->
+    <div slot="body">
+      <form @submit.prevent="onSubmit">
+        <!-- email -->
+        <div class="form-item" :class="{ errorInput: $v.email.$error }">
+          <label>Email:</label>
+          <p class="errorText" v-if="!$v.email.required">Filed is required!</p>
+          <p class="errorText" v-if="!$v.email.email">Email is not correct!</p>
+          <input v-model="email" :class="{ error: $v.email.$error }" @change="$v.email.$touch()">
+        </div>
+
+        <!-- Password -->
+        <div class="form-item" :class="{ errorInput: $v.password.$error }">
+          <label>Password:</label>
+          <p class="errorText" v-if="!$v.password.required">Filed is required!</p>
+          <p
+            class="errorText"
+            v-if="!$v.password.minLength"
+          >Password must have at least {{ $v.password.$params.minLength.min }} !</p>
+          <input
+            type="password"
+            v-model="password"
+            :class="{ error: $v.password.$error }"
+            @change="$v.password.$touch()"
+          >
+        </div>
+        <!-- Repeat password -->
+        <div class="form-item" :class="{ errorInput: $v.repeatPassword.$error }">
+          <label>Repeat password:</label>
+          <p
+            class="errorText"
+            v-if="!$v.repeatPassword.sameAsPassword"
+          >Passwords must be identical!</p>
+          <input
+            type="password"
+            v-model="repeatPassword"
+            :class="{ error: $v.repeatPassword.$error }"
+            @change="$v.repeatPassword.$touch()"
+          >
+        </div>
+        <!-- button -->
+        <button class="btn btnPrimary">Submit!</button>
+      </form>
+    </div>
+  </modal>
+</template>
+
+<script>
+import { required, minLength, email, sameAs } from "vuelidate/lib/validators";
+
+import modal from "@/components/UI/Modal.vue";
+
+export default {
+  components: { modal },
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      repeatPassword: ""
+    };
+  },
+  validations: {
+    name: {
+      required,
+      minLength: minLength(4)
+    },
+    email: {
+      required,
+      email
+    },
+    password: {
+      required,
+      minLength: minLength(6)
+    },
+    repeatPassword: {
+      sameAsPassword: sameAs("password")
+    }
+  },
+  methods: {
+    // ф-ия очистки
+    clearField() {
+        this.name = "";
+        this.email = "";
+        this.password = "";
+        this.repeatPassword = "";
+        this.$v.$reset();
+        this.$emit("close");
+    },
+    onSubmit() {
+      this.$v.$touch();
+      if (!this.$v.$invalid) {
+        const user = {
+          name: this.name,
+          email: this.email,
+          password: this.password
+        };
+        console.log(user);
+
+        // DONE!
+        this.clearField()
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.form-item .errorText {
+  display: none;
+  margin-bottom: 8px;
+  font-size: 13.4px;
+  color: #de4040;
+}
+.form-item {
+  &.errorInput .errorText {
+    display: block;
+  }
+}
+input.error {
+  border-color: #de4040;
+}
+</style>
