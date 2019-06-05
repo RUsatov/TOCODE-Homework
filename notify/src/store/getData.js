7639c2e44c071f229fdc2b794e8ab35e74628dc3
Roman Usatov <R.Usatov76@gmail.com>
2019-06-05 23:25:52 +0300

    added axios in vuex (Notify)

--- notify/src/store/getData.js
+++ notify/src/store/getData.js
@@ -1,4 +1,3 @@
-import axios from 'axios'
 
 export default {
   state: {
@@ -9,31 +8,7 @@ export default {
     }
   },
   actions: {
-    setData({commit}, res) {
-      axios
-        .get('https://tocode.ru/static/c/vue-pro/notifyApi.php')
-          .then(reseponse => {
-            let res = reseponse.data.notify,
-                messages = [],
-                messagesMain = [];
-
-            // filter
-            for (let i = 0; i < res.length; i++) {
-              if (res[i].main) messagesMain.push(res[i])
-              else messages.push(res[i])
-            }
-
-            this.$store.dispatch('setMessage', messages)
-            this.$store.dispatch('setMessageMain', messagesMain)
-          })
-          .catch(error => {
-            console.log(error)
-            this.$store.dispatch('setError', 'Error: Network Error')
-          })
-
-          .finally( () => (this.$store.dispatch('setLoading', false) ))
-      commit();
-    }
+    
   },
   getters: {
     getData(state) {
--- notify/src/store/notify.js
+++ notify/src/store/notify.js
@@ -1,4 +1,5 @@
 import loadMore from '../assets/js/loadMore.js'
+import axios from 'axios'
 
 export default {
   state: {
@@ -7,31 +8,57 @@ export default {
   },
   mutations: {
     setMessage (state, payload) {
-      state.messages = payload
+      state.messages = payload.messages
+      state.messagesMain = payload.messagesMain
     },
-    setMessageMain (state, payload) {
-      state.messagesMain = payload
-    },
+    // setMessageMain (state, payload) {
+    //   state.messagesMain = payload
+    // },
     loadMessages (state, payload) {
       state.messagesMain = [...state.messagesMain, ...payload]
     }
   },
   actions: {
-    setMessage ({commit}, payload) {
-      commit('setMessage', payload)
-    },
-    setMessageMain ({commit}, payload) {
-      commit('setMessageMain', payload)
-    },
+    // setMessage ({commit}, payload) {
+    //   commit('setMessage', payload)
+    // },
+    // setMessageMain ({commit}, payload) {
+    //   commit('setMessageMain', payload)
+    // },
     loadMessages ({commit, getters}) {
       let res = getters.getMessageFilter
       commit('loadMessages', loadMore(res))
+    },
+    setData({commit}) {
+      axios
+        .get('https://tocode.ru/static/c/vue-pro/notifyApi.php')
+          .then(reseponse => {
+            let res = reseponse.data.notify,
+                messages = [],
+                messagesMain = [];
+
+            // filter
+            for (let i = 0; i < res.length; i++) {
+              if (res[i].main) messagesMain.push(res[i])
+              else messages.push(res[i])
+            }
+            commit('setMessage', {messages, messagesMain});
+
+            // this.$store.dispatch('setMessage', messages)
+            // this.$store.dispatch('setMessageMain', messagesMain)
+          })
+          .catch(error => {
+            console.log(error)
+            this.$store.dispatch('setError', 'Error: Network Error')
+          })
+
+          .finally( () => this.$store.dispatch('setLoading', false))
     }
   },
   getters: {
-    getMessage (state) {
-      return state.messages
-    },
+    // getMessage (state) {
+    //   return state.messages
+    // },
     getMessageFilter (state) {
       return state.messages.filter(mes => {
         return mes.main === false
